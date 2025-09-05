8e97961619b07a985d5b07674cd2f2a9bb07ba9c
Modasser Hasan <manvstech2.0@gmail.com>
2025-09-05 22:01:49 +0600

    Reduced Build Size of the Example

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,9 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import cssnano from "cssnano";
+import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    base: "/articwriter/"
+    plugins: [
+        react(),
+        tailwindcss(),
+        viteCompression({ algorithm: "brotliCompress" }),
+    ],
+    base: "/articwriter/",
+    css: {
+        postcss: {
+            plugins: [
+                cssnano({
+                    preset: "default",
+                }),
+            ],
+        },
+    },
+    build: {
+        minify: "terser",
+        terserOptions: {
+            compress: {
+                drop_console: true,
+                drop_debugger: true,
+            },
+        },
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return "vendor"; // splits all node_modules into vendor chunk
+                    }
+                },
+            },
+        },
+        target: "esnext"
+    },
 });
